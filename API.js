e41be5e2fd61a5ae8e8fdeb0b18a26132b20bac8
chocoscoding <oyetit@gmail.com>
2022-01-10 23:51:01 +0100

    first commit

---
+++ API.js
@@ -0,0 +1,5 @@
+const TOTLE = 'ALL FIFA API FOR THE FIFA APP'
+//   https://ratings-api.ea.com/v2/entities/fifa-22-ratings?filter=teamid:(243)&sort=ranking:ASC&limit=40&offset=0
+// https://ratings-api.ea.com/v2/entities/fifa-22-ratings?filter&sort=ranking:ASC&limit=100&offset=0
+// https://ratings-api.ea.com/v2/entities/fifa-22-ratings?filter=primaryKey:165153
+// https://ratings-api.ea.com/v2/entities/fifa-22-ratings?filter=primaryKey:165153
